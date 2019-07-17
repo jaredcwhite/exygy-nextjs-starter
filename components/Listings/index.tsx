0d2bdcf3f53fc04f6ac2382547666585f3f678ea
Jared White <jared@jaredwhite.com>
2019-07-17 05:11:46 +0000

    Add Typescript support

---
+++ components/Listings/index.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+
+export interface Listing {
+  id: number;
+  name: string;
+  featured?: boolean;
+}
+
+interface ListingsProps {
+  listings: Array<Listing>;
+}
+
+export default function(props: ListingsProps) {
+  const listings = props.listings
+
+  const listItems = listings.map(listing =>
+    <li key={listing.id}>
+      <Link href="listings/[id]" as={`/listings/${listing.id}`}>
+        <a>
+          {listing.name}
+          {listing.featured &&
+            <span style={{marginLeft: '20px'}}><em>Featured</em></span>
+          }
+        </a>
+      </Link>
+    </li>
+  )
+
+  return (
+    <ul>{listItems}</ul>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Layout from '../layouts/application';
+import Listings, { Listing } from '../components/Listings'
+
+// Messing around with different ways of adding in data
+let listings: Array<Listing> = []
+listings.push({id: 3, name: "First Listing!"})
+listings.push({id: 6, name: "Second Listing", featured: true})
+let thirdListing = {id: 12, name: "Third Listing"}
+listings.push(thirdListing)
+
+const Index = () => (
+  <Layout>
+    <p>Hello Next.js</p>
+    <Listings listings={listings} />
+    <hr/>
+  </Layout>
+)
+
+export default Index
