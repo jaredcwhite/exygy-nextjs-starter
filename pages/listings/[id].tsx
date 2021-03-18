a5a0deac504bfd078604cbdfae038c6db852d539
Jared White <jared@jaredwhite.com>
2021-03-17 20:22:55 -0700

    inspect context for SSR

--- pages/listings/[id].tsx
+++ pages/listings/[id].tsx
@@ -1,27 +1,24 @@
 import Head from "next/head"
-import { useRouter } from "next/router"
 import Layout from "../../layouts/application.js"
 
 export default function Listing(props: any) {
-  const router = useRouter();
-
   return (
     <Layout>
       <Head>
-        <title>Listing {router.query.id}</title>
+        <title>Listing {props.data.id}</title>
       </Head>
 
-      <h1>Listing Id: {router.query.id}</h1>
-      <p>This is the listing content. {props.data}</p>
+      <h1>Listing Id: {props.data.id}</h1>
+      <p>This is the listing content… {props.data.randomNumber}</p>
     </Layout>
   );
 }
 
-// This gets called on every request
-export async function getServerSideProps() {
-  // Fetch data from external API
-  const data = (Math.random() * 10000).toFixed()
+export async function getServerSideProps(context: any) {
+  const num = (Math.random() * 10000).toFixed()
 
-  // Pass data to the page via props
-  return { props: { data } }
+  return { props: { data: {
+    id: context.params.id,
+    randomNumber: num
+  }}}
 }
